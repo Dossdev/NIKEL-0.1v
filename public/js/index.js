e030d81f6d884b7d076f0821b92cc2af176bf91f
Matheus Catarina <100724863+Dossdev@users.noreply.github.com>
2023-01-25 14:48:01 -0300

    1

--- public/js/index.js
+++ public/js/index.js
@@ -36,7 +36,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
 document.getElementById("create-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const email = document.getElementById("email-create-input").value;
+    const emailCreateInput = document.getElementById("email-create-input").value;
     const senha = document.getElementById("password-create-input").value;
 
     if (email.length < 5) {
