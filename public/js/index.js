b266b37cf6914bd09abeeae17ce699c9a04f846b
Matheus Catarina <100724863+Dossdev@users.noreply.github.com>
2023-01-25 14:44:13 -0300

    pass js

--- public/js/index.js
+++ public/js/index.js
@@ -4,59 +4,60 @@ const session = localStorage.getItem("session");
 
 checkLogged();
 
-//LOGIN NO SISTEMA
+//LOGAR NO SISTEMA
 document.getElementById("login-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email-input").value;
-    const password = document.getElementById("password-input").value;
-    const checkSession = document.getElementById("session-check").checked;
+    const senha = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-input").checked;
 
     const account = getAccount(email);
 
     if (!account) {
-        alert("Opps! Verifique o usuário ou a senha.")
+        alert("Opps! Verifique o usuário ou a senha!");
         return;
     }
+
     if (account) {
-        if (account.password != password) {
-            alert("Opps! Verifique o usuário ou a senha.")
+        if (account.password !== senha) {
+            alert("Opps! Verifique o usuário ou a senha!");
             return;
         }
 
         saveSession(email, checkSession);
 
         window.location.href = "home.html";
+
     }
-})
+});
 
 //CRIAR CONTA
 document.getElementById("create-form").addEventListener("submit", function (e) {
     e.preventDefault();
-    alert("enviou o form");
 
     const email = document.getElementById("email-create-input").value;
-    const passwordCreateInput = document.getElementById("password-create-input").value;
+    const senha = document.getElementById("password-create-input").value;
 
     if (email.length < 5) {
-        alert("Preencha o campo com um e-mail válido");
+        alert("Preencha o campo com um e-mail válido!")
         return;
     }
 
-    if (password.length < 4) {
-        alert("Preencha a senha com no mínimo 4 dígitos.")
+    if (senha.length < 4) {
+        alert("A senha deve conter no mínimo 4 dígitos")
         return;
     }
 
     saveAccount({
         login: email,
-        password: password,
+        password: senha,
         transactions: []
     });
 
     myModal.hide();
 
-    alert("Conta criada com sucesso!");
+    alert("Conta criada com sucesso, parabéns!!!!!");
 });
 
 function checkLogged() {
@@ -64,9 +65,10 @@ function checkLogged() {
         sessionStorage.setItem("logged", session);
         logged = session;
     }
+
     if (logged) {
         saveSession(logged, session);
-        window.location.href = "home.html"
+        window.location.href = "home.html";
     }
 }
 
